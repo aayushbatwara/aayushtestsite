7476197216226b1019c109fb057590cf9b66d5bb
Aayush <aayushbatwara@yahoo.com>
2021-08-21 22:23:35 +0800

    new

---
+++ habits.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$conn=mysqli_connect('sophia.cs.hku.hk', 'abatwara', '93211390', 'abatwara') 
+or die ('Error! '.mysqli_connect_error($conn)); // connect to database
+
+// get data from database
+$query = "SELECT * FROM habitTracking";
+$result = mysqli_query($conn,$query) or die("Error! " . mysql_error($conn)); // Execute query
+
+$row = mysqli_fetch_array($result);
+echo $row; 
+
+mysqli_free_result($result);
+mysqli_close($conn);  
+
+
+?>
