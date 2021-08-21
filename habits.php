c9ce89656c42cbf1b91956683cecb4b4711749a8
Aayush <aayushbatwara@yahoo.com>
2021-08-21 22:24:49 +0800

    new

--- habits.php
+++ habits.php
@@ -1,18 +1,19 @@
 <?php
 
+echo "Hello!";
 
-$conn=mysqli_connect('sophia.cs.hku.hk', 'abatwara', '93211390', 'abatwara') 
-or die ('Error! '.mysqli_connect_error($conn)); // connect to database
+// $conn=mysqli_connect('sophia.cs.hku.hk', 'abatwara', '93211390', 'abatwara') 
+// or die ('Error! '.mysqli_connect_error($conn)); // connect to database
 
-// get data from database
-$query = "SELECT * FROM habitTracking";
-$result = mysqli_query($conn,$query) or die("Error! " . mysql_error($conn)); // Execute query
+// // get data from database
+// $query = "SELECT * FROM habitTracking";
+// $result = mysqli_query($conn,$query) or die("Error! " . mysql_error($conn)); // Execute query
 
-$row = mysqli_fetch_array($result);
-echo $row; 
+// $row = mysqli_fetch_array($result);
+// echo $row; 
 
-mysqli_free_result($result);
-mysqli_close($conn);  
+// mysqli_free_result($result);
+// mysqli_close($conn);  
 
 
 ?>
